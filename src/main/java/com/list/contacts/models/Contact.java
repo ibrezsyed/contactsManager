963f23a034a71540fa6b516bcc4bcf1a840228ee
unknown <syed.ibrez@thesyndicate.tech>
2019-04-24 16:09:37 +0530

    hibernate config

--- src/main/java/com/list/contacts/models/Contact.java
+++ src/main/java/com/list/contacts/models/Contact.java
@@ -12,6 +12,63 @@ public class Contact {
     @Id
     @GeneratedValue( strategy = GenerationType.AUTO)
     private int id;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getSocialProfile() {
+        return socialProfile;
+    }
+
+    public void setSocialProfile(String socialProfile) {
+        this.socialProfile = socialProfile;
+    }
+
     private String firstName;
     private String lastName;
     private String email;
---
+++ src/main/java/com/list/contacts/mvcconfig/HibbernateConfig.java
@@ -0,0 +1,59 @@
+package com.list.contacts.mvcconfig;
+
+import com.list.contacts.base.ReadProperties;
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class HibbernateConfig {
+
+    @Autowired
+    private ReadProperties readProperties;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.list.contacts.models");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(readProperties.getDbDriverclassName());
+        dataSource.setUrl(readProperties.getDbUrl());
+        dataSource.setUsername(readProperties.getDbUserName());
+        dataSource.setPassword(readProperties.getPassword());
+
+        return dataSource;
+    }
+
+    /*@Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager
+                = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }*/
+
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty(
+                "hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty(
+                "hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+
+        return hibernateProperties;
+    }
+}
